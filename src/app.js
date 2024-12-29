1aca8cdba19056a76473b54169aad12cabfe1364
UmerAbbasi-100 <k213929@nu.edu.pk>
2024-12-29 13:30:59 +0500

    routes testing...

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app=express();
 
+app.use("/test/1",(req,res)=>{
+    res.send("test/2////")
+})
+app.use("/test",(req,res)=>{
+    res.send("test p request aii thi...")
+})
+
 app.use("/hello",(req,res)=>{
     res.send("Hello World");
 })
+app.use("/",(req,res)=>{
+    res.send("sabko handle kryga.....")
+});
 app.listen(3000,()=>{
     console.log("server is running on : 3000");;
 });
