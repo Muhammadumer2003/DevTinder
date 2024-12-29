c6498c705bce6fa701992414eb32f1c813e197f2
UmerAbbasi-100 <k213929@nu.edu.pk>
2024-12-29 14:01:23 +0500

    routes testing...

--- src/app.js
+++ src/app.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const app=express();
 
-app.use("/test/1",(req,res)=>{
-    res.send("test/2////")
-})
-app.use("/test",(req,res)=>{
-    res.send("test p request aii thi...")
-})
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello World");
+
+app.use("/user",(req,res)=>{
+    res.send("wah bawa gg!!!");
 })
-app.use("/",(req,res)=>{
-    res.send("sabko handle kryga.....")
+//Http methods routes testing
+app.get("/user",(req,res)=>{
+    res.send({
+        name: "John Doe",
+        age: 25,
+        city: "New York"
+    })
 });
+
+app.post("/user",(req,res)=>{
+    res.send("post api call");
+});
+
+app.patch("/user",(req,res)=>{
+    res.send("patch api call");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("delete api call");
+});
+
+//route testing
+// app.use("/test/1",(req,res)=>{
+//     res.send("test/2////")
+// })
+// app.use("/test",(req,res)=>{
+//     res.send("test p request aii thi...")
+// })
+
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello World");
+// })
+// app.use("/",(req,res)=>{
+//     res.send("sabko handle kryga.....")
+// });
 app.listen(3000,()=>{
     console.log("server is running on : 3000");;
 });
